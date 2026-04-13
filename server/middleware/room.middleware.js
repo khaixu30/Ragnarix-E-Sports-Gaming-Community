0c22700db6f2fdd61e89f6d3f13229feda206358
ShahCodista <maliha.shah2006@gmail.com>
2026-04-13 14:27:11 +0500

    room.routes

---
+++ server/middleware/room.middleware.js
@@ -0,0 +1,25 @@
+import pool from "../db/db";
+
+const adminAccessMiddleware = async(req, res, next)=> {
+    try {
+        const user_id = req.user._id;
+        const {room_id} = req.params;
+        const room = await pool.query(
+            "SELECT * FROM rooms WHERE admin_id = $1 AND id = $2",
+            [user_id, room_id]
+        )
+        if(rooms.rows.length===0){
+            return res.status(400).json({success: false, message: "Unauthorized."})
+        }
+
+        next();
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Internal server error."
+        });
+    }
+}
+
+export default adminAccessMiddleware
