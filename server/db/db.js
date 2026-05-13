69fe3bcccd6f47fdd1a284998f093914272467ea
khaixu30 <ahmed30.iftikhar@gmail.com>
2026-05-13 22:49:14 +0500

    NeonDB Added

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-    host: "localhost",
-    password: "admin123",
-    user: "postgres",
-    database: "fragmint_db"
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
